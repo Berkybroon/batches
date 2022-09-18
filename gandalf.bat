6ef16d1c96cdb4931d4a89ac41cc18f0a7b0def3
Berkybroon <113478240+Berkybroon@users.noreply.github.com>
2022-09-18 01:17:19 +0100

    Add files via upload

--- gandalf.bat
+++ gandalf.bat
@@ -1,8 +1,5 @@
 curl -o "C:\gandalf.bmp" https://raw.githubusercontent.com/Berkybroon/batches/main/l-intro-1662053031.bmp
-timeout 1
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\gandalf.bmp" /f
-timeout 1
 taskkill /IM explorer.exe /f
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
 explorer.exe
-timeout 1
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
