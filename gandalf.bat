f685eac9e426fcce9c3fd2cccb385f10f3be6e23
Berkybroon <113478240+Berkybroon@users.noreply.github.com>
2022-09-18 02:35:22 +0100

    Add files via upload

--- gandalf.bat
+++ gandalf.bat
@@ -3,3 +3,5 @@ reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\
 taskkill /IM explorer.exe /f
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
 explorer.exe
+timeout 2
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
