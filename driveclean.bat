495864147b1c666b266d50f0d0bc2579c0391160
Berkybroon <113478240+Berkybroon@users.noreply.github.com>
2023-07-04 14:10:49 +0100

    Updated 04/07/2023
    Now deletes Windows search cache, allows larger pagefile, and iterates through users to clear individual temp folders.

--- driveclean.bat
+++ driveclean.bat
@@ -1,13 +1,22 @@
 net stop wuauserv
 powercfg.exe /hibernate off
-timeout 1
-del %systemdrive%\$Recycle.bin\* /q /f /s
+sc config wsearch start= disabled
+net stop wsearch
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows Search" /v SetupCompletedSuccessfully /t REG_DWORD /d 0 /f
+del "%ProgramData%\Microsoft\Search\Data\Applications\Windows\Windows.edb"
+del "%ProgramData%\Microsoft\Search\Data\Applications\Windows\Windows.db"
+:wsearch
+sc config wsearch start= delayed-auto
+net start wsearch
+IF NOT %ERRORLEVEL%==0 (goto :wsearch) ELSE goto :END
+:END
+for /d %%F in (c:\users\*) do del "%%F\appdata\local\temp\*" /s /q
+del %systemdrive%\$Recycle.Bin\* /q /f /s
+del %windir%\Temp\* /q /f /s
 del %temp%\* /q /f /s
-del %Windir%\SoftwareDistribution\Download\* /q /f /s
-del C:\Windows\prefetch\*.* /q /f /s
-del "%systemroot%\SoftwareDistribution\DataStore\Logs\edb.log"
-timeout 1
+del %windir%\SoftwareDistribution\Download\* /q /f /s
+del %windir%\prefetch\*.* /q /f /s
+del "%windir%\SoftwareDistribution\DataStore\Logs\edb.log"
 wmic computersystem where name="%computername%" set AutomaticManagedPagefile=False
-timeout 1
-wmic pagefileset where name="C:\\pagefile.sys" set InitialSize=2048,MaximumSize=4096
+wmic pagefileset where name="C:\\pagefile.sys" set InitialSize=4096,MaximumSize=8192
 net start wuauserv
